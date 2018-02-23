e59ca8d9ff2822735783f8c1b714b66bfc65ecc6
Tran Hong Duong <hdtran@etud.insa-toulouse.fr>
2018-02-23 15:53:02 +0100

    update action.php

---
+++ 03-WebService-Bis/action.php
@@ -0,0 +1,51 @@
+<?php
+//require_once("hashValues.php");
+
+
+if ($_GET && isset($_GET['cmd']) /*&& isset($_GET['prepend'])*/) {
+	header("Content-Type: application/json");
+	$cmd = $_GET['cmd'];
+	//$prepend = $_GET['prepend'];
+	$hash = hash("sha1", /*$prepend . */$cmd);
+	$path = realpath("./onetimepad");
+	passthru($path . " " . $hash,$exit_code);
+	if ($exit_code === 0){
+		echo json_encode(["msg"=>"Command executed","success"=>true]);
+	}
+	elseif ($exit_code === 1) {
+		echo json_encode(["msg"=>"Command disabled","success"=>false]);	
+	}
+	else {
+		echo json_encode(["msg"=>"Internal error","success"=>false]);
+	}
+
+	/*
+	if (array_search($hash, $hashValues)){
+		//passthru("/usr/bin/python send.py ". $cmd);
+		if ($hash === $hashValues['stop']){
+			echo json_encode(["msg"=>"Command disabled","success"=>false]);
+		}else {
+			echo json_encode(["msg"=>"Command executed","success"=>true]);
+		}
+	}
+	*/
+	/*
+	if ($cmd==="START") {
+		//passthru("/usr/bin/python send.py START");
+		echo json_encode(["msg"=>"Command executed","success"=>true]);
+	}
+	else if ($cmd==="STOP") {
+		//passthru("/usr/bin/python send.py STOP");
+		echo json_encode(["msg"=>"Command disabled","success"=>false]);
+	}
+	else if ($cmd==="CHANGE_BGCOLOR") {
+		//passthru("/usr/bin/python send.py CHANGE_BGCOLOR");
+		echo json_encode(["msg"=>"Command executed","success"=>true]);
+	}
+	else if ($cmd==="CHANGE_FGCOLOR") {
+		//passthru("/usr/bin/python send.py CHANGE_FGCOLOR");
+		echo json_encode(["msg"=>"Command executed","success"=>true]);
+	}
+	*/
+}
+else system("whoami");
---
+++ 03-WebService-Bis/hashValues.php
@@ -0,0 +1,8 @@
+<?php
+
+$hashValues = array("start"=>"0d9c277d67b705a5cfcb83801c45d8b6263dbe5fd1e2aa2cbcc828f6f00f8cbb",
+	"stop"=>"a606183840f0af6335a621210ccaa98cab0cdc8669a515b2ba37f28043f7a3dd",
+	"CHANGE_BG"=>"d0dedfd5b0e08929080cd21862d02e0323acf93084b25040970b316e553c00ab",
+	"CHANGE_FG"=>"b7ca6867dbb5fc26592ffd6b396e87a67fe1374085dce1e5f1cb73cdc31fd6a6");
+
+?>
