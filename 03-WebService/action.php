de85c4c440593059f930f440e395ae7da1608be0
duong1749635 <hdtran23111995@gmail.com>
2018-03-04 19:01:01 +0100

    ajouter des comments et nettoyer action.php

--- 03-WebService/action.php
+++ 03-WebService/action.php
@@ -1,8 +1,12 @@
 <?php
 
-// on suppose que cmd est commandeZESECRET
+// $authorizedCommands contient tous les hachés authorisés de cmd 
 $authorizedCommands = ["974a38236b1d9e6383d9ead3b4ca0a8d8117c0f4","22982f3eb0b0516cdef93053eae620c55f73d93f","2ec1adfb530ecbbce6d23b2cca46f4e41ce5d54c"];
 
+/*
+* si le haché du "cmd" est dans $authorizedCommands, 
+* le command va être chiffré et envoyer vers andruino pour executer.
+*/
 if ($_GET && isset($_GET['cmd'])) {
 	header("Content-Type: application/json");
 	$cmd = hash("sha1",$_GET['cmd']);
@@ -13,21 +17,5 @@ if ($_GET && isset($_GET['cmd'])) {
 		echo json_encode(["msg"=>"Command executed","success"=>true]);
 	}
 	else echo json_encode(["msg"=>"Command disabled","success"=>false]);
-	/*if ($cmd==="START") {
-		passthru("/usr/bin/python send.py START");
-		echo json_encode(["msg"=>"Command executed","success"=>true]);
-	}
-	else if ($cmd==="STOP") {
-		passthru("/usr/bin/python send.py STOP");
-		echo json_encode(["msg"=>"Command disabled","success"=>false]);
-	}
-	else if ($cmd==="CHANGE_BGCOLOR") {
-		passthru("/usr/bin/python send.py CHANGE_BGCOLOR");
-		echo json_encode(["msg"=>"Command executed","success"=>true]);
-	}
-	else if ($cmd==="CHANGE_FGCOLOR") {
-		passthru("/usr/bin/python send.py CHANGE_BGCOLOR");
-		echo json_encode(["msg"=>"Command executed","success"=>true]);
-	}*/
 }
 else system("whoami");
