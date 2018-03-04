daeedab4e06847bc5c0796b98816dbee64c3632c
duong1749635 <hdtran23111995@gmail.com>
2018-03-04 19:31:21 +0100

    Update listen.py

--- 03-WebService/listen.py
+++ 03-WebService/listen.py
@@ -1,6 +1,7 @@
 import serial,time
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
+# Chaque seconde, lire le temps revoie par andruino pour la synchornisation
 while True:
 	text_file = open('compteur','w+')
 	text_file.write(ser.readline())
